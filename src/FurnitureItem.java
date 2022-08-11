85672f2bf25010bb9e279e304900b78b5ec56053
Monali Sharma <mona.chaudhari444@gmail.com>
2022-08-11 11:54:02 +0530

    declaring attributes

---
+++ src/FurnitureItem.java
@@ -0,0 +1,11 @@
+public class FurnitureItem {
+
+    int furnitureCode;
+    String furnitureType;
+    String gradeOfFurniture;
+    String color;
+    String furnitureUsage;
+    double price;
+
+}
+
